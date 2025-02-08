22359135de3a346ea05cf3a345eba899c06015fd
nriosvillaco <nicholasrios1213@gmail.com>
2025-02-08 15:19:42 -0500

    added code to save final clean and processed dfs to directory

--- code/data_prep/data_prep.R
+++ code/data_prep/data_prep.R
@@ -6,7 +6,8 @@ library(lubridate)
 #load data ----
 
 #create file path
-seriea_23_24 <- here("data/a_23-24.csv")
+root <- here()
+seriea_23_24 <- here("data/raw/a_23-24.csv")
 
 data <- read.csv(seriea_23_24)
 
@@ -18,3 +19,7 @@ source(data_cleaning)
 data_features <- here("code/data_prep/source/data_features.R")
 source(data_features)
 
+#save cleaned and processed dfs
+save(data_23_24, file = paste0(root, "/data/clean/data__matches_23_24"))
+save(data_bet_23_24, file = paste0(root, "/data/clean/data_sportsbook_23_24"))
+save(data_aggregate, file = paste0(root, "/data/processed/data_aggregated_matches_23_24"))
